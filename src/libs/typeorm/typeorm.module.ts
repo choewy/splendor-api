90d588f202310820ee176d3c395fcbc36304d5be
choewy <choewy32@gmail.com>
2024-03-05 17:23:44 +0900

    feature/typeorm injectable repository

--- src/libs/typeorm/typeorm.module.ts
+++ src/libs/typeorm/typeorm.module.ts
@@ -1,6 +1,10 @@
 import { ConfigExModule, TypeOrmConfigService } from '@libs/config';
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, FactoryProvider, Module, Type } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+import { AbstractRepository } from './abstracts';
+import { INJECTABLE_REPOSITORY_METADATA_KEY } from './decorators';
 
 @Module({})
 export class TypeOrmExModule {
@@ -16,6 +20,28 @@ export class TypeOrmExModule {
         }),
       ],
       module: TypeOrmExModule,
+    };
+  }
+
+  static forFeature<IRepository extends AbstractRepository<any>>(Repositories: Type<IRepository>[]): DynamicModule {
+    const repositoryProviders: FactoryProvider[] = [];
+
+    for (const Repository of Repositories) {
+      repositoryProviders.push({
+        inject: [DataSource],
+        provide: Repository,
+        useFactory(dataSource: DataSource) {
+          const target = Reflect.getMetadata(INJECTABLE_REPOSITORY_METADATA_KEY, Repository);
+
+          return new Repository(target, dataSource);
+        },
+      });
+    }
+
+    return {
+      module: TypeOrmExModule,
+      providers: repositoryProviders,
+      exports: repositoryProviders,
     };
   }
 }
