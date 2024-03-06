b2ad919bf72cb94433bfd723842825596551e4eb
choewy <choewy32@gmail.com>
2024-03-06 13:17:54 +0900

    feature/mock abstract repository

--- src/libs/typeorm/abstracts/abstract-repository.ts
+++ src/libs/typeorm/abstracts/abstract-repository.ts
@@ -1,8 +1,26 @@
+import { FactoryProvider, Type } from '@nestjs/common';
 import { DataSource, EntityTarget, ObjectLiteral, Repository } from 'typeorm';
 
 export abstract class AbstractRepository<E extends ObjectLiteral> extends Repository<E> {
+  static mock<AbstractMockRepository extends AbstractRepository<any>>(MockRepository: Type<AbstractMockRepository>): FactoryProvider {
+    return {
+      provide: MockRepository,
+      useFactory() {
+        const mockRepository = new MockRepository(null);
+        const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(mockRepository));
+        const mock = {};
+
+        for (const method of methods) {
+          mock[method] = () => null;
+        }
+
+        return mock as AbstractMockRepository;
+      },
+    };
+  }
+
   constructor(readonly target: EntityTarget<E>, readonly dataSource: DataSource) {
-    super(target, dataSource.createEntityManager());
+    super(target, dataSource?.createEntityManager());
   }
 
   get transaction() {
