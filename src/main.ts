3b0662694fde9db7cf150fb73f66c04560fe1ddc
choewy <choewy32@gmail.com>
2024-03-06 14:11:33 +0900

    feature/global class serializer, class validator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { SwaggerExModule } from '@libs/swagger';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
@@ -7,6 +8,24 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   SwaggerExModule.setup(app);
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      enableImplicitConversion: true,
+      enableCircularCheck: true,
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+        enableCircularCheck: true,
+      },
+    }),
+  );
 
   await app.listen(4000);
 }
