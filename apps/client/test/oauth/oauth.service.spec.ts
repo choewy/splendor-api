ccd2060fa78b949584a4f0cf711e20a58230bb83
choewy <choewy32@gmail.com>
2024-03-10 16:57:30 +0900

    feature/test oauthService getOAuthToken

--- apps/client/test/oauth/oauth.service.spec.ts
+++ apps/client/test/oauth/oauth.service.spec.ts
@@ -1,10 +1,10 @@
 import { ClientJwtService } from '@apps/client/jwt';
 import { CreateOAuthUrlCommand } from '@apps/client/oauth/commands';
-import { CreateGoogleOAuthUrlDto, CreateKakaoOAuthUrlDto, CreateNaverOAuthUrlDto } from '@apps/client/oauth/dtos';
+import { CreateGoogleOAuthUrlDto, CreateKakaoOAuthUrlDto, CreateNaverOAuthUrlDto, OAuthStateDto } from '@apps/client/oauth/dtos';
 import { OAuthService } from '@apps/client/oauth/oauth.service';
 import { OAuthPlatform, OAuthRepository, UserRepository } from '@libs/entity';
 import { TestingFixture, TestingRepository } from '@libs/testing';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -57,6 +57,56 @@ describe(OAuthService.name, () => {
 
       expect(dto).toBeInstanceOf(CreateNaverOAuthUrlDto);
       expect(dto.url.startsWith('https://nid.naver.com/oauth2.0/authorize')).toBeTruthy();
+    });
+  });
+
+  describe('getOAuthToken', () => {
+    const state = TestingFixture.of(OAuthStateDto);
+
+    let getGoogleOAuthToken: jest.SpyInstance<Promise<string>, [code: string]>;
+    let getKakaoOAuthToken: jest.SpyInstance<Promise<string>, [code: string]>;
+    let getNaverOAuthToken: jest.SpyInstance<Promise<string>, [code: string, state: OAuthStateDto]>;
+
+    beforeEach(() => {
+      if (getGoogleOAuthToken) {
+        getGoogleOAuthToken.mockClear();
+      }
+
+      if (getKakaoOAuthToken) {
+        getKakaoOAuthToken.mockClear();
+      }
+
+      if (getNaverOAuthToken) {
+        getNaverOAuthToken.mockClear();
+      }
+
+      getGoogleOAuthToken = jest.spyOn(service, 'getGoogleOAuthToken').mockResolvedValue('');
+      getKakaoOAuthToken = jest.spyOn(service, 'getKakaoOAuthToken').mockResolvedValue('');
+      getNaverOAuthToken = jest.spyOn(service, 'getNaverOAuthToken').mockResolvedValue('');
+    });
+
+    it('platform이 google인 경우 Google OAuth Token만 요청해야 한다.', async () => {
+      await service.getOAuthToken(OAuthPlatform.Google, '', state);
+
+      expect(getGoogleOAuthToken).toHaveBeenCalledTimes(1);
+      expect(getKakaoOAuthToken).toHaveBeenCalledTimes(0);
+      expect(getNaverOAuthToken).toHaveBeenCalledTimes(0);
+    });
+
+    it('platform이 kakao인 경우 Kakao OAuth Token만 요청해야 한다.', async () => {
+      await service.getOAuthToken(OAuthPlatform.Kakao, '', state);
+
+      expect(getGoogleOAuthToken).toHaveBeenCalledTimes(0);
+      expect(getKakaoOAuthToken).toHaveBeenCalledTimes(1);
+      expect(getNaverOAuthToken).toHaveBeenCalledTimes(0);
+    });
+
+    it('platform이 naver인 경우 Naver OAuth Token만 요청해야 한다.', async () => {
+      await service.getOAuthToken(OAuthPlatform.Naver, '', state);
+
+      expect(getGoogleOAuthToken).toHaveBeenCalledTimes(0);
+      expect(getKakaoOAuthToken).toHaveBeenCalledTimes(0);
+      expect(getNaverOAuthToken).toHaveBeenCalledTimes(1);
     });
   });
 });
