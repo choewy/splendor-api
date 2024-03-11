0d4e194beb73833db1795131c50fe601a8fad242
최원영 <70950533+choewy@users.noreply.github.com>
2024-03-11 10:07:54 +0900

    Update user.controller.ts

--- apps/client/src/user/user.controller.ts
+++ apps/client/src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { ApiController } from '@libs/swagger';
 
 import { UserService } from './user.service';
 
-@ApiController('users', '유저')
+@ApiController('users', '회원')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 }
