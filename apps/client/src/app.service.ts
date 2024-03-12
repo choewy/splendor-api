7a5d18e0d94653a71487c67588532d55c4031157
choewy <choewy32@gmail.com>
2024-03-12 23:52:34 +0900

    feature/app service, controller

--- apps/client/src/app.service.ts
+++ apps/client/src/app.service.ts
@@ -1,8 +1,13 @@
+import { VersionDto } from '@libs/common';
+import { APP_CONFIG, AppConfigReturnType } from '@libs/configs';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  getVersion() {
+    return new VersionDto('client', this.configService.get<AppConfigReturnType>(APP_CONFIG).version);
   }
 }
