e522bf0902ce35438d5f92c058c4832689fd097c
choewy <choewy32@gmail.com>
2024-03-12 23:54:35 +0900

    feature/app controllers

--- apps/admin/src/app.controller.ts
+++ apps/admin/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { VersionDto } from '@libs/common';
+import { ApiController } from '@libs/swagger';
+import { Get } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
-@Controller()
+@ApiController('', 'App')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOkResponse({ type: VersionDto })
   getVersion() {
     return this.appService.getVersion();
   }
