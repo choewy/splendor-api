6379a7253220852093774a6f01ed2c5c02495bd3
choewy <choewy32@gmail.com>
2024-03-12 18:24:00 +0900

    feature/common libs

---
+++ libs/common/src/enums.ts
@@ -0,0 +1,8 @@
+export enum KafkaClientTopics {}
+
+export enum KafkaStreamTopics {
+  Stream = 'stream',
+  Play = 'stream.play',
+  Donate = 'stream.donate',
+  StudioUpdate = 'stream.studio_update',
+}
