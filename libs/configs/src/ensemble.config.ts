1498ef49b5fc89036498c39dc183f3af3a9e9a37
choewy <choewy32@gmail.com>
2024-03-13 16:25:31 +0900

    refactor/env key names

--- libs/configs/src/ensemble.config.ts
+++ libs/configs/src/ensemble.config.ts
@@ -14,9 +14,9 @@ export const EnsembleConfig = registerAs(
   ENSEMBLE_CONFIG,
   (): EnsembleConfigReturnType => ({
     servers: {
-      client: process.env.ENSEMBLE_CLIENT_URL,
-      stream: process.env.ENSEMBLE_STREAM_URL,
-      admin: process.env.ENSEMBLE_ADMIN_URL,
+      client: process.env.ENSEMBLE_CLIENT_SERVER_URL,
+      stream: process.env.ENSEMBLE_STREAM_SERVER_URL,
+      admin: process.env.ENSEMBLE_ADMIN_SERVER_URL,
     },
   }),
 );
