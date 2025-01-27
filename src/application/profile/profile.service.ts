eb5ab1c8aa3e164b6835e67ca07a9a4ba6e50514
choewy <choewy32@gmail.com>
2025-01-28 01:17:55 +0900

    feature: profile module

--- src/application/profile/profile.service.ts
+++ src/application/profile/profile.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  // TODO 내 프로필 조회
+  async get() {
+    return;
+  }
+
+  // TODO 내 프로필 수정
+  async update() {
+    return;
+  }
+}
