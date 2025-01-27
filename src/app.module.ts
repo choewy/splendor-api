602719f5fe7618ab40ba2650d4f89c24bb37d381
choewy <choewy32@gmail.com>
2025-01-28 01:15:10 +0900

    feature: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './application/auth/auth.module';
 import { PlayModule } from './application/play/play.module';
 import { PlayerModule } from './application/player/player.module';
 import { RoomModule } from './application/room/room.module';
 
 @Module({
-  imports: [PlayerModule, RoomModule, PlayModule],
+  imports: [AuthModule, PlayerModule, RoomModule, PlayModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/application/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  // TODO 회원가입(카카오)
+  async signup() {
+    return;
+  }
+
+  // TODO 로그인(카카오)
+  async login() {
+    return;
+  }
+}
