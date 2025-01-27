f6e7d36b510d9729be71e02521a58f04f4fe391f
choewy <choewy32@gmail.com>
2025-01-28 01:09:27 +0900

    feature: room module

---
+++ src/application/room/room.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { RoomService } from './room.service';
+
+@Controller('rooms')
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+}
